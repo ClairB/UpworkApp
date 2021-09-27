d35f1091ab67f5a01c56d9817c056951db799865
Gina Saffy <Gina_Saffy22@hotmail.com>
2021-09-27 12:23:49 +0000

    Updated App.js by GB

--- src/App.js
+++ src/App.js
@@ -8,6 +8,12 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          <div id="like_button_container"></div>
+          <script src="https://unpkg.com/react@17/umd/react.development.js" crossorigin></script>
+          <script src="https://unpkg.com/react-dom@17/umd/react-dom.development.js" crossorigin></script>
+          <script src="like_button.js"></script>
+          const domContainer = document.querySelector('#like_button_container');
+          ReactDOM.render(e(LikeButton), domContainer);
         </p>
         <a
           className="App-link"
