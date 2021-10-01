508ea6a0771ce59d0e7f52225ba4d8572b666afc
Gina Saffy <Gina_Saffy22@hotmail.com>
2021-10-01 07:18:22 +0000

    Updated App.js changed text

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          My Second React Push
+          My Third React Push
         </a>
       </header>
     </div>
